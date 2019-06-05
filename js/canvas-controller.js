a47b45760b9a416c985ee1c18d3a1b0efbfc775c
Ellah Toren <ellahtoren@gmail.com>
2019-06-05 15:59:59 +0300

    update-interval

--- js/canvas-controller.js
+++ js/canvas-controller.js
@@ -1,6 +1,7 @@
 'use strict'
 let gCanvas;
 let gCtx;
+let gDrawing;
 
 
 function init() {
@@ -11,14 +12,21 @@ function init() {
     gCanvas.height = window.innerHeight - 150;
 
     gCanvas.addEventListener("mousedown", onDraw);
+    gCanvas.addEventListener("mouseup", stopInterval);
 }
 
 function onDraw(ev) {
-    let 
-    console.log('clicked canvas');
+    gDrawing = setInterval(function (){
+        console.log('clicked canvas');
+    },100);
+    
     // gets shape from user acc to option input
     // gets color from user
     // switch: in case 'square'/'circle'
+}
+
+function stopInterval() {
+    clearInterval(gDrawing);
 }
 
 function drawSquare(color) {
